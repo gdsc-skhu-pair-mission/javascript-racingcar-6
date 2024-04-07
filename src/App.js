ec9de8f334edddfa04a1d1692ac2b4e397a9c213
SEYEON-PARK <fourleaf123@naver.com>
2024-04-07 21:54:11 +0900

    Feat : 시도할 횟수 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import { Randoms, Console } from '@woowacourse/mission-utils';
+import { Random, Console } from "@woowacourse/mission-utils";
 
 async function inputCarName() {
   let carNameList = await Console.readLineAsync(
-    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
   );
 
   if (!Number.isNaN(Number(carNameList))) {
-    throw new Error('[Error] 숫자가 입력되었습니다. 문자를 입력해주십시오.');
+    throw new Error("[Error] 숫자가 입력되었습니다. 문자를 입력해주십시오.");
   }
-  if (!carNameList.includes(',')) {
+  if (!carNameList.includes(",")) {
     throw new Error("[Error] ',' 가 포함되어 있지 않습니다.");
   }
 
-  let carName = carNameList.split(',');
+  let carName = carNameList.split(",");
 
   carName.forEach((name) => {
     if (name.length > 5) {
-      throw new Error('[Error] 5자 이하인 이름으로 입력해주세요.');
+      throw new Error("[Error] 5자 이하인 이름으로 입력해주세요.");
     }
   });
+
+  return carName;
 }
 
-class App {
-  async play() {}
+async function inputTryCount() {
+  let tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+  if (Number.isNaN(Number(tryCount))) {
+    throw new Error("[Error] 숫자가 아닙니다.");
+  }
+  if (tryCount < 0) {
+    throw new Error("[Error] 0보다 큰 수를 입력해야 합니다.");
+  }
+
+  return tryCount;
 }
+class App {
+  async play() {
+    let carName = await inputCarName();
+    let tryCount = await inputTryCount();
+
+    console.log(carName);
+    console.log(tryCount);
+  }
+}
+
+const game = new App();
+game.play();
 
 export default App;
