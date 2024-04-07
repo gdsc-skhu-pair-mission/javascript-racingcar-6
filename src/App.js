02617136f9614a55abd03239f116b043ec23fe35
Younghyun Kim <dana_03@naver.com>
2024-04-07 21:33:42 +0900

    fetch: 자동차 이름 입력받는 함수

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
+import { Randoms, Console } from '@woowacourse/mission-utils';
+
+async function inputCarName() {
+  let carNameList = await Console.readLineAsync(
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  );
+
+  if (!Number.isNaN(Number(carNameList))) {
+    throw new Error('[Error] 숫자가 입력되었습니다. 문자를 입력해주십시오.');
+  }
+  if (!carNameList.includes(',')) {
+    throw new Error("[Error] ',' 가 포함되어 있지 않습니다.");
+  }
+
+  let carName = carNameList.split(',');
+
+  carName.forEach((name) => {
+    if (name.length > 5) {
+      throw new Error('[Error] 5자 이하인 이름으로 입력해주세요.');
+    }
+  });
+}
+
 class App {
   async play() {}
 }
