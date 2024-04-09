0bc93f400d459d3f77a7eca7ab5c9306c86bd02c
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-04-09 16:00:05 +0900

    Add: 자동차 유효성 검사 추가

--- src/validators/CarNameValidator.js
+++ src/validators/CarNameValidator.js
@@ -1,14 +1,17 @@
 export default function getAvailableCars(CarArray) {
   CarArray.forEach((car) => {
     if (car.trim() === '') {
-      throw new Error('[ERROR]공백을 입력하셨습니다.');
+      throw new Error('[ERROR] 공백만 입력하시면 안됩니다.');
+    }
+    if (car.includes(' ')) {
+      throw new Error('[ERROR] 이름 사이에 공백을 입력하시면 안됩니다.');
     }
     if (car.length > 5) {
-      throw new Error('[ERROR]이름이 5글자를 초과하셨습니다.');
+      throw new Error('[ERROR] 이름이 5글자를 초과하시면 안됩니다.');
     }
   });
   if (CarArray.length < 2) {
-    throw new Error('[ERROR]자동차가 한 대이하로 입력하셨습니다. ');
+    throw new Error('[ERROR] 자동차를 1대 이하로 입력하시면 안됩니다.');
   }
   return CarArray;
 }
--- src/view/input/AttemptInput.js
+++ src/view/input/AttemptInput.js
@@ -4,7 +4,6 @@ import getAvailableAttempts from '../../validators/AttemptValidator.js';
 export default async function askForAttempts() {
   const attemptRound =
     await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-  // eslint-disable-next-line no-use-before-define
   const roundNumber = getAvailableAttempts(attemptRound);
   return roundNumber;
 }
