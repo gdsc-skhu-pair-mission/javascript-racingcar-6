8e181ea779ebc333781e3b042accf56d9ddab440
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-04-09 17:39:29 +0900

    Modify: 테스트 통과를 위한 유효성 검사 변경

--- src/validators/AttemptValidator.js
+++ src/validators/AttemptValidator.js
@@ -6,8 +6,8 @@ export default function getAvailableAttempts(Round) {
   if (Number.isNaN(roundNumber)) {
     throw new Error('[ERROR] 숫자가 아닌 값을 입력하시면 안됩니다.');
   }
-  if (roundNumber < 2) {
-    throw new Error('[ERROR] 1 이하의 수를 입력하시면 안됩니다.');
+  if (roundNumber < 1) {
+    throw new Error('[ERROR] 0 이하의 수를 입력하시면 안됩니다.');
   }
   return roundNumber;
 }
